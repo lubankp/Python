5b9fd7659d15b8744441d78fa05a4d748f541001
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-05-30 18:00:17 +0200

    modules connection

--- py6.py
+++ py6.py
@@ -1,33 +1,4 @@
-
-
-
-def days_to_units(num_of_days, conversion_unit):
-    if conversion_unit == "hours":
-        return f"{num_of_days} days are {num_of_days*24} hours"
-    elif conversion_unit == "minutes":
-        return f"{num_of_days} days are {num_of_days*24*60} minutes"
-    else:
-        return "unsupported unit"
-
-def validate_and_execute():
-    
-    try:
-
-        num_of_days = int(days_and_unit_dictionary["days"])
-
-        # We want to make convertion only to positives digits
-        if num_of_days > 0:
-            result = days_to_units(num_of_days,days_and_unit_dictionary["unit"])
-            print(result)
-        elif num_of_days == 0:
-            print( "You entered 0")
-        else:
-            print("You entered negative number")
-        
-        
-    except ValueError:
-        print("Your input is no valid number")
-
+import helper
 
 input_data = " "
 while input_data != "exit":
@@ -37,4 +8,4 @@ while input_data != "exit":
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
     print(days_and_unit_dictionary)
 
-    validate_and_execute()
+    helper.validate_and_execute(days_and_unit_dictionary)
