3984d88e4ceb7ef5dfbcc14fd90f3d121a22e378
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-05-30 21:07:21 +0200

    modify_ods_file_ready

--- modify_ods_file.py
+++ modify_ods_file.py
@@ -1,7 +1,10 @@
-from pyexcel_ods import get_data, save_data
 from collections import OrderedDict
+import pyexcel_ods as pe
 
-data = OrderedDict()
-data.update({"Sheet1":[[1,2,3],[4,5,6]]})
-data.update({"Sheet2":[["row1", "row2", "row3"]]})
-save_data("Spreadsheet.ods", data)
+sheet = pe.get_data("Desktop/Python kurs/Spreadsheet.ods")
+print(sheet)
+
+file2 = OrderedDict()
+file2.update({"Sheet1": [[1,2,3],[1,5,6]]})
+file2.update({"Sheet2": [["row1","row2","row3"],[1,5,6]]})
+pe.save_data("Desktop/Python kurs/Spreadsheet2.ods", file2) 
