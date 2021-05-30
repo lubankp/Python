9d7f1f78c530d80249c097e39a8e3f0b2778ab18
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-05-30 19:26:19 +0200

    datetime module

---
+++ time_till_deadline.py
@@ -0,0 +1,15 @@
+import datetime
+
+user_input = input("enter your goal with a deadline seperated by coln \n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+deadline_date = datetime.datetime.strptime(deadline,"%d.%m.%Y")
+#calculation how many days flom now to deadline
+
+today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
+
+print(f"Dear user time till your goal: {goal} is {time_till.days} days")
