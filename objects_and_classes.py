bb69a6bc8ff26a466896832ef6cf7c0e3f6636a0
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-06-24 13:00:42 +0200

    #second class post

--- objects_and_classes.py
+++ objects_and_classes.py
@@ -1,4 +1,5 @@
 import user
+from post import Post
 
 app_user_one = user.User("nn@nn.com", "Nana Jas", "pwd1", "DevOps enginer")
 
@@ -8,3 +9,6 @@ app_user_one.get_user_info()
 
 app_user_two = user.User("jj@jj.com", "Jaja Noka", "pwd2", "DevOps trainer")
 app_user_two.get_user_info()
+
+new_post = Post("on a secret mission today", app_user_two.name)
+new_post.get_post_info()
