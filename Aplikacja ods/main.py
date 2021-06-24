7554c6e60a4fe06f12f38e780f5561733bcba2f4
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-06-24 18:30:03 +0200

    #execute application with buttons

--- Aplikacja ods/main.py
+++ Aplikacja ods/main.py
@@ -8,7 +8,24 @@ def about():
 
 gui = tk.Tk()
 gui.title("Przykładowa aplikacja")
-gui.geometry("200x100")
-btn = tk.Button(gui, command=about, text="Kliknij mnie!")
-btn.grid(row=0, column=0)
+gui.geometry("1500x900")
+
+butt_ref_start = tk.Button(gui, command=about, text="rozpoczecie ref", fg="black", font = "Verdana 24", bd = 2, bg = "blue", relief = "groove", width = 14, height = 2)
+butt_ref_start.grid(row=1, column=1)
+butt_ref_end = tk.Button(gui, command=about, text="Zakoncenie ref", fg="black", font = "Verdana 24", bd = 2, bg = "pink", relief = "groove", width = 14, height = 2)
+butt_ref_end.grid(row=1, column=4)
+
+butt_prod = tk.Button(gui, command=about, text="Produkcja", fg="black", font = "Verdana 36", bd = 2, bg = "blue", relief = "groove", width = 10, height = 2)
+butt_prod.grid(row=3, column=1, padx=5, pady=5)
+butt_prze = tk.Button(gui, command=about, text="Przezbrojenie", fg="black", font = "Verdana 36", bd = 2, bg = "pink", relief = "groove", width = 10, height = 2)
+butt_prze.grid(row=3, column=2, padx=5, pady=5)
+butt_pomi = tk.Button(gui, command=about, text="Pomiary", fg="black", font = "Verdana 36", bd = 2, bg = "red", relief = "groove", width = 10, height = 2)
+butt_pomi.grid(row=3, column=3, padx=5, pady=5)
+butt_awa = tk.Button(gui, command=about, text="Awaria", fg="black", font = "Verdana 36", bd = 2, bg = "orange", relief = "groove", width = 10, height = 2)
+butt_awa.grid(row=3, column=4, padx=5, pady=5)
+butt_przes = tk.Button(gui, command=about, text="Przestoj", fg="black", font = "Verdana 36", bd = 2, bg = "violet", relief = "groove", width = 21, height = 2)
+butt_przes.grid(row=4, column=1, padx=5, pady=5, columnspan=2)
+butt_uwag = tk.Button(gui, command=about, text="Uwagii", fg="black", font = "Verdana 36", bd = 2, bg = "green", relief = "groove", width = 21, height = 2)
+butt_uwag.grid(row=4, column=3, padx=5, pady=5, columnspan=2)
+
 gui.mainloop()
