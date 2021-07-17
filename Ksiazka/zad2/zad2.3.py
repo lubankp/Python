5851a3ac0ddd6d37f472fb2273717d3087d46570
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-07-17 16:03:36 +0200

    adding zad2.3

---
+++ Ksiazka/zad2/zad2.3.py
@@ -0,0 +1,23 @@
+from random import randint
+
+def min(list):
+    min = 101
+    for i in list:
+        if min > int(i):
+           min = int(i)
+    return min
+
+def max(list):
+    max = 0
+    for i in list:
+        if max < int(i):
+           max = int(i)
+    return max
+
+list = []
+for i in range(0,10):
+    list.append(randint(1,100))
+
+print(list)
+print(min(list))
+print(max(list))
