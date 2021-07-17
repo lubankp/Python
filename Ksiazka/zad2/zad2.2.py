b78189e20d4b753e7197c5031589b4348f61bfb4
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-07-17 16:19:18 +0200

    zad2.2 second version of reverse fun

--- Ksiazka/zad2/zad2.2.py
+++ Ksiazka/zad2/zad2.2.py
@@ -4,8 +4,15 @@ def reverse(list1):
         list2[-i-1] = el
     return list2
 
-list = list(range(1,10))
+def reverse2(list1, count):
+    list2 = list1[-1:-count:-1]
+    return list2
+
+count = 10
+list = list(range(1,count))
 
 print(list)
 list_rev = reverse(list)
 print(list_rev)
+list_rev2 = reverse2(list, count)
+print(list_rev2)
