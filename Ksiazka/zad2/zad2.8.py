60c3a659f06b4cabc29a5d93aede603d5243bc97
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-07-23 17:16:55 +0200

    add zad2.8

--- Ksiazka/zad2/zad2.8.py
+++ Ksiazka/zad2/zad2.8.py
@@ -1,1 +1,42 @@
-# Morse code second wersion
+# Guess secret code
+
+from random import randint
+
+
+def guess_code(correct):
+    cor = 0
+    for x, el2 in enumerate(guess_number):
+        find = 0
+
+        for i, el in enumerate(random_code):
+           # print (f"{el2}  {el}")
+            if int(el) == int(el2):
+                if i == x:
+                    print(f"{el2} is in correct place")
+                    find += 1
+                    cor +=1
+                    break
+                else:
+                    #print(f"g_num {guess_number.count(el2)} r_num {random_code.count(el)}")
+                    if guess_number.count(el2) > random_code.count(el):
+                        print(f"Too many {el2}")
+                        find += 1
+                    else:
+                        print(f"{el2} is in not in correct place")
+                        find += 1
+                    break
+        if find == 0:
+            print(f"{el2} is in not in code")
+
+    return cor
+
+
+random_code = [randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9)]
+
+print(random_code)
+correct = 0
+while correct != 4:
+    guess_number = input("Guess a code: \n")
+    correct = guess_code(correct)
+
+print("Congratulation!")
