ed6d10997a793070b3ee8e1f1151682f3812ed32
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-07-24 17:14:07 +0200

    zad3.6 - clip fun - list comprehension

---
+++ Ksiazka/zad3/zad3.6.py
@@ -0,0 +1,27 @@
+# clip function list comprehension
+
+from random import randint
+
+def clip(x):
+    if x < 1:
+        return 1
+    elif x > 10:
+        return 10
+    else:
+        return x
+
+def clip_list(list):
+    list2 = [clip(x) for x in list]
+
+    return list2
+
+
+number = int(input("Enter a number: \n"))
+number2 = clip(number)
+
+print(number2)
+
+list3 = [randint(-10,50), randint(-10,50),randint(-10,50),randint(-10,50),randint(-10,50)]
+print(list3)
+list4 = clip_list(list3)
+print(list4)
