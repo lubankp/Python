fea5380cf810d8b8665fd31d8aba3b2e79e02343
pawel <pawel.lubanski.91@gmail.com>
2021-11-21 18:16:24 +0100

    added project 1, randomowe serca

--- Ksiazka/Project1/serca.py
+++ Ksiazka/Project1/serca.py
@@ -5,33 +5,44 @@ import random
 t = turtle.Turtle()
 
 
-def circle(radius, number):
+def serce(radius, number):
     t.penup()
     t.fd(radius)
+    t.rt(180)
     t.pendown()
     b = 360/number
     a = (180-b)/2
     c = radius*(math.sin(math.radians(b)))/(math.sin(math.radians(a)))
     t.rt(90+90-a)
 
-    for _ in range(number):
+    for _ in range(60):
         t.fd(c)
         t.rt(180-2*a)
+    t.rt(180)
+    for _ in range(60):
+        t.fd(c)
+        t.rt(180-2*a)
+
+    d = radius * 3
+    t.fd(d)
+    t.rt(105)
+    t.fd(d)
+
 
 t.hideturtle()
 t.speed(0)
 
-for _ in range(20):
+for _ in range(15):
     t.pensize(random.randint(1,20))
     r = random.randint(0,10)/10
     g = random.randint(0, 10) / 10
     b = random.randint(0, 10) / 10
     t.color(r,g,b)
-    x = 100 * random.randint(0,5)
-    y = 100 * random.randint(0,5)
+    x = 100 * random.randint(-5,5)
+    y = 100 * random.randint(-5,5)
     t.penup()
     t.goto(x,y)
     t.pendown()
-    circle(50, 100)
+    serce(50, 100)
 
 t.showturtle()
